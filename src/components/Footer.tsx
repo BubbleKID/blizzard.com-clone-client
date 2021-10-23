3932e7f472e721a30fe177fc0a4d2ba24c54106f
Mark <fqchenxin@gmail.com>
2021-10-23 23:54:52 +1100

    update footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,13 @@ import React from "react";
 interface LinkProps {
     name: string;
     url: string;
+    fontSize: number;
 }
 
 const Link = (props: LinkProps) => {
     return (
         <React.Fragment >
-            <a href={props.url}>{props.name}</a>
+            <a style={{textTransform: "uppercase", fontSize: props.fontSize}} href={props.url}>{props.name}</a>
         </React.Fragment>
     );
 };
@@ -25,10 +26,24 @@ export default function Footer() {
         { name: 'Site Map', url: '#'}
     ];
 
+    const smallLinks = [
+        { name: 'Privacy', url: '#'},
+        { name: 'Legal', url: '#'},
+        { name: 'Terms', url: '#'},
+        { name: 'Cookie Policy', url: '#'},
+        { name: 'Cookie Setting', url: '#'}
+    ];
+
     return (
         <div>
             {links.map(link => (
-                <Link key={link.name} name={link.name} url={link.url}></Link>
+                <Link key={link.name} name={link.name} url={link.url} fontSize={14}></Link>
+            ))}
+            <p>All games, one app:Battle.net Desktop App</p>
+            <p>©2021 BLIZZARD ENTERTAINMENT, INC. ALL RIGHTS RESERVED.</p>
+            <p>All trademarks referenced herein are the properties of their respective owners.</p>
+            {smallLinks.map(link => (
+                <Link key={link.name} name={link.name} url={link.url} fontSize={12}></Link>
             ))}
         </div>
     );
