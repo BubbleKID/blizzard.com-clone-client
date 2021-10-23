39dcdefe7d60f22d2b7a0154e30cd43049c6ede0
Mark <fqchenxin@gmail.com>
2021-10-23 21:58:45 +1100

    init repo

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+let array: string[];
+
+let Welcome = () => { 
+  return <h1>Hello</h1>; 
+}
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome/>
+        Hello World 
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MyComponent/>, document.getElementById('root'));
