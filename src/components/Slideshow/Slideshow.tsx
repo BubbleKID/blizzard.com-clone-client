ad46a6019eefba99770e56fd9ce855ff598734d3
Mark <fqchenxin@gmail.com>
2021-10-25 22:54:17 +1100

    update slideshow

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Slideshow.css';
 
 interface Props {
+    slides: Slide[]
+}
+
+interface Slide {
     icon: string,
     iconWidth: number,
     title: string,
     btnTest: string,
-    backgroundImage: string
+    backgroundImage: string,
+    backgroundColor: string
 }
 
 export default function Slideshow(props: Props) {
+    const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0);
     return (
         <React.Fragment>
-            <div className='slide-container'>
-                <div className='slide-content' style={{ backgroundImage: `url(${props.backgroundImage})`}}>                <img className='slide-icon' width={props.iconWidth} src={props.icon}/>
-                    <span className='slide-title'>{props.title}</span>
-                    <button className='slide-btn' type='button'>{props.btnTest}</button>
+            <div className='slide-container' style={{ backgroundImage: `url(${props.slides[currentSlideIndex] .backgroundImage})`, backgroundColor: props.slides[currentSlideIndex].backgroundColor}}>
+                <div className='slide-content'>                
+                    <img className='slide-icon' width={props.slides[currentSlideIndex] .iconWidth} src={props.slides[currentSlideIndex] .icon}/>
+                    <span className='slide-title'>{props.slides[currentSlideIndex] .title}</span>
+                    <button className='slide-btn' type='button'>{props.slides[currentSlideIndex] .btnTest}</button>
                 </div>
             </div>
-            <div className='slider'></div>
+            <div className='slider'>
+                { props.slides.map((slide, index) => <div key={index} onClick={() => setCurrentSlideIndex(index)} className={(index === currentSlideIndex) ? 'slider-item active':'slider-item'}></div>) }
+            </div>
         </React.Fragment>
     )
 }
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -6,14 +6,44 @@ import Section from '../components/Section';
 import desktopBg from '../images/DesktopBG_Image.jpeg';
 import carrersBg from '../images/careers-1.jpeg';
 import './Home.css';
-import backgroundImage from '../images/wow-bcc-overlords-banner-desktop-2500x514.jpeg';
+import backgroundImageTbc from '../images/wow-bcc-overlords-banner-desktop-2500x514.jpeg';
+import backgroundImageD2 from '../images/D2R_2021_Blizzard.comDesktop_2500x514_MB01.png';
+import backgroundImageOW from '../images/OW_2021_HalloweenTerror_BlizzardHomepage-Desktop_2500x514_JP.jpeg';
 import tbcIcon from '../images/icons/WOW_Classic_Phase_2p5p2_Logo_01.png';
+import d2Icon from '../images/icons/D2R_DiabloResurrected_Logo_blizz.png';
+import owIcon from '../images/icons/OW_2021_HalloweenTerror_Logo-Only_enUS.png';
 
+const slides = [
+    {
+        icon: tbcIcon,
+        iconWidth: 260,
+        btnTest: 'Play Today',
+        title: 'Illidan\'s lieutenants\nAwait',
+        backgroundImage: backgroundImageTbc,
+        backgroundColor: 'rgb(30, 3, 8)'
+    },
+    {
+        icon: d2Icon,
+        iconWidth: 260,
+        btnTest: 'Buy Now',
+        title: 'Evil Is Resurrected',
+        backgroundImage: backgroundImageD2,
+        backgroundColor: 'rgb(0, 0, 0)'
+    },
+    {
+        icon: owIcon,
+        iconWidth: 260,
+        btnTest: 'Buy Now',
+        title: 'Buy 50 Loot Boxes\nAnd Get 10 Bonus!',
+        backgroundImage: backgroundImageOW,
+        backgroundColor: 'rgb(35, 38, 51)'
+    }
+]
 export default function Home() {
     return(
         <div>
             <Menu/>
-            <Slideshow icon={tbcIcon} iconWidth={295} btnTest={'Play Today'} title={"Illidan\'s lieutenants\nAwait "} backgroundImage={backgroundImage}/>
+            <Slideshow slides={slides}/>
             <Gallery/>
             <Section backgroundImage={desktopBg}/>
             <Section backgroundImage={carrersBg}/>
