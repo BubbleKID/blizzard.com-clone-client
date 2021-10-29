ba52bfe61b05e2c54df821c5675e4ee074a1e456
Mark <fqchenxin@gmail.com>
2021-10-29 18:05:21 +1100

    update slideshow animation

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Slideshow.css';
 import  { ReactComponent as PreBtn } from '../../images/Blizzard Entertainment-10.svg';
 import  { ReactComponent as NexBtn } from '../../images/Blizzard Entertainment-11.svg';
@@ -21,16 +21,25 @@ export default function Slideshow(props: Props) {
     let [seconds, setSeconds] = useState<number>(3);
     const [slideControlButtonOpacity, setSlideControlButtonOpacity] = useState<number>(0);
     const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0);
+    const [shiftClassName, setShiftClassName] = useState<string>('shift-left-fade-in');
+    const ref = useRef<HTMLButtonElement>(null);
+
     const onClickSlideControlBtn = (type: string): void => {
         let newIndex;
       
+        ref.current?.classList.remove('shift-left-fade-in');
+        void ref.current?.offsetWidth;
         if(type === 'prev') {
+            setShiftClassName('shift-left-fade-in');
+            void ref.current?.offsetWidth;
             if(currentSlideIndex === 0) {
                 newIndex = props.slides.length - 1;
             } else {
                 newIndex = currentSlideIndex - 1;
             }
         } else {
+            setShiftClassName('shift-left-fade-in');
+            void ref.current?.offsetWidth;
             if(currentSlideIndex === props.slides.length - 1) {
                 newIndex = 0;
             } else {
@@ -38,6 +47,7 @@ export default function Slideshow(props: Props) {
             }
         }
         setCurrentSlideIndex(newIndex);
+        
     }
     const isPlay = false;
 
@@ -60,7 +70,7 @@ export default function Slideshow(props: Props) {
         },
         [running]
     );
-
+    
     return (
         <React.Fragment>
             <div 
@@ -68,16 +78,16 @@ export default function Slideshow(props: Props) {
                 onMouseEnter={() => {setSlideControlButtonOpacity(1); setRunning(false) }}
                 onMouseLeave={() => {setSlideControlButtonOpacity(0); setRunning(true) }}
             >
-                <div className='slide-background'  style={{ backgroundImage: `url(${props.slides[currentSlideIndex] .backgroundImage})`, backgroundColor: props.slides[currentSlideIndex].backgroundColor}}>
+                <div className='slide-background' style={{ backgroundImage: `url(${props.slides[currentSlideIndex] .backgroundImage})`, backgroundColor: props.slides[currentSlideIndex].backgroundColor}}>
                     <div className="gradient-container"></div>
                 </div>
-                    <div className='slide-content' >                
+                    <div className='slide-content'>                
                         <img className='slide-icon' width={props.slides[currentSlideIndex] .iconWidth} src={props.slides[currentSlideIndex] .icon}/>
-                        <span className='slide-title'>{props.slides[currentSlideIndex] .title}</span>
-                        <button className='slide-btn' type='button'>{props.slides[currentSlideIndex] .btnTest}</button>
+                        <span className='slide-title'>{props.slides[currentSlideIndex].title}</span>
+                        <button ref={ref} className={'slide-btn ' + shiftClassName} type='button'>{props.slides[currentSlideIndex].btnTest}</button>
                     </div>
                     <button className="slide-control-btn prev" style={{opacity: slideControlButtonOpacity}} onClick={() => onClickSlideControlBtn('prev')}><PreBtn/></button>
-                    <button className="slide-control-btn next" style={{opacity: slideControlButtonOpacity}} onClick={() =>  onClickSlideControlBtn('next')}><NexBtn/></button>
+                    <button className="slide-control-btn next" style={{opacity: slideControlButtonOpacity}} onClick={() => onClickSlideControlBtn('next')}><NexBtn/></button>
             </div>
             <div className='slider'>
                 { 
